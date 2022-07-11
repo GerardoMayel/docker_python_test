cc00e05957807751e5af6910717630a7987f5024
Gerardo Mayel Fernández Alamilla <geramfernandez@gmail.com>
2022-07-10 23:12:41 -0500

    docker file, python app file

---
+++ app/hola_mundo.py
@@ -0,0 +1,12 @@
+import pandas as pd
+print('hola mundo')
+
+dict = {'col1': [1, 2]}
+
+df = pd.DataFrame.from_dict(dict)
+
+print(df)
+
+# pip freeze > requirements.txt
+# Python 3.8.3
+# python ./hola_mundo.py
